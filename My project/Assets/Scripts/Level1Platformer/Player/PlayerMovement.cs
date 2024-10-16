8f62f9cc5a294a2086fcf327232077e40d48dca4
Максим Майнагашев <maxuser99@mail.ru>
2024-10-17 04:28:32 +0700

    Basic movement comolete

--- My project/Assets/Scripts/Level1Platformer/Player/PlayerMovement.cs
+++ My project/Assets/Scripts/Level1Platformer/Player/PlayerMovement.cs
@@ -12,11 +12,15 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float _speed;
     [SerializeField] private float _jumpForce;
+    private float _sprintMultiplier = 2;
     private GroundDetector _groundDetector;
     private Vector3 _direction;
     private Animator _animator;
     private SpriteRenderer _spriteRenderer;
     private Rigidbody2D _rigidbody2D;
+    private float _currentSpeed;
+    private bool _isLedderIsNear;
+    private bool _isClimbing;
     
     private void Start()
     {
@@ -24,47 +28,119 @@ public class PlayerMovement : MonoBehaviour
         _spriteRenderer = GetComponent<SpriteRenderer>();
         _rigidbody2D = GetComponent<Rigidbody2D>();
         _groundDetector = GetComponent<GroundDetector>();
+        _isClimbing = false;
     }
 
     private void FixedUpdate()
     {
-        StartJump();
         Movement();
+        CheckFalling();
     }
 
     private void Movement()
     {
-        _direction = Vector3.zero;
-        if (Input.GetKey(KeyCode.A))
+        if (_isClimbing == false)
         {
-            _direction = Vector3.left;
-            if (_direction.x < 0)
+            _currentSpeed = _speed;
+            _direction = Vector3.zero;
+            if (Input.GetKey(KeyCode.A))
             {
-                _spriteRenderer.flipX = false;
+                _direction = Vector3.left;
+                if (_direction.x < 0)
+                {
+                    _spriteRenderer.flipX = false;
+                }
             }
-        }
-        else if (Input.GetKey(KeyCode.D))
-        {
-            _direction = Vector3.right;
-            if (_direction.x > 0)
+            else if (Input.GetKey(KeyCode.D))
             {
-                _spriteRenderer.flipX = true;
+                _direction = Vector3.right;
+                if (_direction.x > 0)
+                {
+                    _spriteRenderer.flipX = true;
+                }
             }
-        }
 
-        _direction *= _speed;
-        _direction.y = _rigidbody2D.velocity.y;
-        _rigidbody2D.velocity = _direction;
-        _animator.SetFloat("Speed", Mathf.Abs(_direction.x));
+            if (Input.GetKey(KeyCode.Space))
+            {
+                _animator.SetTrigger("StartJump");
+            }
+
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                _currentSpeed *= _sprintMultiplier;
+            }
+
+            _direction *= _currentSpeed;
+            _direction.y = _rigidbody2D.velocity.y;
+            _rigidbody2D.velocity = _direction;
+            _animator.SetFloat("Speed", Mathf.Abs(_direction.x));
+            
+            if (Input.GetKeyUp(KeyCode.E) && _isLedderIsNear) // Check for E key press near ladder
+            {
+                _animator.SetBool("IsClimbing", true);
+                _isClimbing = true;
+                Debug.Log("Stick");
+            }
+        }
+        else
+        {
+            Climbing();
+        }
     }
 
-    private void StartJump()
+    private void Climbing()
     {
-        if (Input.GetKey(KeyCode.W) && _groundDetector.IsGrounded == true)
+        _rigidbody2D.gravityScale = 0f;
+        if (Input.GetKeyUp(KeyCode.E)) // Detach from ladder on E press
         {
-            Debug.Log("Jump!");
-            _rigidbody2D.AddForce(Vector2.up * _jumpForce, ForceMode2D.Impulse);
-            //_animator.SetTrigger(Jump);
+            StopClimbing();
+        }
+        else if (Input.GetKeyDown(KeyCode.W)) // Climb up on W press
+        {
+            _rigidbody2D.velocity = new Vector2(0f, _speed);
+            _animator.SetFloat("ClimbSpeed", _speed);
+        }
+        else if (Input.GetKeyDown(KeyCode.S)) // Climb up on W press
+        {
+            _rigidbody2D.velocity = new Vector2(0f, _speed);
+            _animator.SetFloat("ClimbSpeed", _speed);
+        }
+    }
+
+    private void StopClimbing()
+    {
+        _animator.SetBool("IsClimbing", false);
+        _isClimbing = false;
+        _rigidbody2D.gravityScale = 1f;
+    }
+
+    private void CheckFalling()
+    {
+        _animator.SetBool("IsGrounded", _groundDetector.IsGrounded);
+    }
+
+    private void Jump()
+    {
+        _rigidbody2D.AddForce(Vector2.up * _jumpForce, ForceMode2D.Impulse);
+        _animator.SetBool("StartJump", false);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.GetComponent<Ledder>())
+        {
+            _isLedderIsNear = true;
+            Debug.Log("Ledger is near");
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.GetComponent<Ledder>())
+        {
+            _isLedderIsNear = false;
+            Debug.Log("Exit ledder");
+            StopClimbing();
         }
     }
 }
